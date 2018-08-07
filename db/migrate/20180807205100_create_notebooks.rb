bba4804e3dad8ab1e10d171a3c1789b5273513f2
mark <m.arklee@outlook.com>
2018-08-07 14:25:53 -0700

    Finish migration of users, notes, notebooks, tags, taggings

---
+++ db/migrate/20180807205100_create_notebooks.rb
@@ -0,0 +1,10 @@
+class CreateNotebooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :notebooks do |t|
+      t.string :title, null:false
+      t.integer :author_id, null:false
+      t.timestamps
+    end
+    add_index :notebooks, :author_id
+  end
+end
---
+++ db/migrate/20180807205114_create_taggings.rb
@@ -0,0 +1,10 @@
+class CreateTaggings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :taggings do |t|
+      t.integer :tag_id, null:false
+      t.integer :note_id, null:false
+      t.timestamps
+    end
+    add_index :taggings, [:tag_id, :note_id], unique:true
+  end
+end
