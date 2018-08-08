4704a74fed9e0e2a2ea23cbfb070d2b534bc8195
mark <m.arklee@outlook.com>
2018-08-07 20:42:28 -0700

    Add store.js in store folder

--- frontend/letterNote.jsx
+++ frontend/letterNote.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import RootReducer from './reducers/root_reducer';
-
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = document.getElementById("root");
