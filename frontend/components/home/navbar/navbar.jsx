04fc18a6001a4b6723333413f595a56e358308d4
mark <m.arklee@outlook.com>
2018-08-09 17:44:23 -0700

    Add fake links on homepage

--- frontend/components/home/navbar/navbar.jsx
+++ frontend/components/home/navbar/navbar.jsx
@@ -1,23 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class NavBar extends React.Component {
 	render() {
 		return <div className="nav-div">
 				<div className="logo">
-					<p className="logo-letter"><img className="home-logo-image" src="http://logok.org/wp-content/uploads/2015/02/Evernote-logo.png" />
+					<p className="logo-letter">
+						<img className="home-logo-image" src="http://logok.org/wp-content/uploads/2015/02/Evernote-logo.png" />
 						LETTERNOTE
 					</p>
 				</div>
 
-        <div className="other-stuff2">
-
-        </div>
-        <div className="other-stuff2">
-
-        </div>
-        <div className="other-stuff2">
-
-        </div>
+				<div className="other-stuff2">
+					<a className="link-google" href="https://www.w3schools.com">
+						w3schools
+					</a>
+				</div>
+				<div className="other-stuff2">
+					<a className="link-google" href="https://www.w3schools.com">
+						w3schools
+					</a>
+				</div> 
+				<div className="other-stuff2">
+					<a className="link-google" href="https://www.w3schools.com">
+						w3schools
+					</a>
+				</div>
 			</div>;
 	}
 }
