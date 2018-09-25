70525ec0488e50cc9c9bb33db735d7e8fd214a12
mark <m.arklee@outlook.com>
2018-09-25 13:02:33 -0700

    focus disabled

--- frontend/components/home/focus/focus.jsx
+++ frontend/components/home/focus/focus.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 
 class Focus extends React.Component {
   render() {
-    return <div className='focus-splash'>
-      <div className='focus-image-div'>
-        <div className='focus-image'></div>
-      </div>
+    // return <div className='focus-splash'>
+    //   <div className='focus-image-div'>
+    //     <div className='focus-image'></div>
+    //   </div>
 
-      <div className='focus-text'>mark</div>
-      <div className='focus-text'>mark</div>
-      <div className='focus-text'>mark</div>
-    </div>;
+    //   <div className='focus-text'>mark</div>
+    //   <div className='focus-text'>mark</div>
+    //   <div className='focus-text'>mark</div>
+    // </div>;
+    return <div></div>;
   }
 }
 
