cd446d21cd45877a131793970b9a7cae5357f2a3
mark <m.arklee@outlook.com>
2018-10-17 15:48:32 -0700

    Working on Focus and Links

--- frontend/components/home/focus/focus.jsx
+++ frontend/components/home/focus/focus.jsx
@@ -2,16 +2,30 @@ import React from 'react';
 
 class Focus extends React.Component {
   render() {
-    // return <div className='focus-splash'>
-    //   <div className='focus-image-div'>
-    //     <div className='focus-image'></div>
-    //   </div>
+    return <div className="focus-splash">
+				<div className="focus-image-div">
+					<div className="focus-image" />
+				</div>
 
-    //   <div className='focus-text'>mark</div>
-    //   <div className='focus-text'>mark</div>
-    //   <div className='focus-text'>mark</div>
-    // </div>;
-    return <div></div>;
+				<div>
+					<h1>Focus on what matters most</h1>
+				</div>
+				<div className="focus-text">
+					<span>
+						Manage everything from big projects to personal moments.
+					</span>
+				</div>
+				<div className="focus-text">
+					<span>
+						Capture ideas and inspiration in notes, voice, and pictures.
+					</span>
+				</div>
+				<div className="focus-text">
+					<span>
+            Never lose track of your tasks and deadlines.
+          </span>
+				</div>
+			</div>;
   }
 }
 
---
+++ frontend/components/home/links/links.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Links extends React.Component {
+  render() {
+    return <div className="focus-splash">
+      <div className="focus-image-div">
+        <div className="focus-image" />
+      </div>
+
+      <div>
+        <h1>Focus on what matters most</h1>
+      </div>
+      <div className="focus-text">
+        <span>
+          Manage everything from big projects to personal moments.
+					</span>
+      </div>
+      <div className="focus-text">
+        <span>
+          Capture ideas and inspiration in notes, voice, and pictures.
+					</span>
+      </div>
+      <div className="focus-text">
+        <span>
+          Never lose track of your tasks and deadlines.
+          </span>
+      </div>
+    </div>;
+  }
+}
+
+
+export default Links;
